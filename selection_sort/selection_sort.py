c5a4f2a2875c328a825024247ef5d6658ffd4b5e
Atina <kellieatina@gmail.com>
2025-06-20 14:01:21 +0300

    First Trial

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,17 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    def selection_sort(arr):
+        for i in range(len(arr)-1):
+            #We will assume the minimum element is the first element
+            min_index = i
+            #Comparing the minimum elemet with the rest of the elements
+            for j in range(i+1 , len(arr)):
+                #We check if current element is less than the element we marked as the minimum
+                if arr[j] < arr[min_index]:
+                    #If true, value is reassigned 
+                    min_index = j
+                # Swapping the elements
+                temp = arr[i]
+                arr[i] = arr[min_index]
+                arr[min_index]= temp
+        return arr
+
